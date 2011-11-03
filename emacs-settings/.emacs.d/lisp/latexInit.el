d2ba10e62645a67b7b188a550bf81401e53871d0
Pat O'Keefe <patokeefe1@gmail.com>
2011-11-02 21:36:53 -0400

    Created orgtbl translator for latex matrices

--- emacs-settings/.emacs.d/lisp/latexInit.el
+++ emacs-settings/.emacs.d/lisp/latexInit.el
@@ -15,17 +15,58 @@
 (defun flymake-get-tex-args (file-name)
   (list "/usr/texbin/pdflatex" (list "-shell-escape" "-file-line-error" "-draftmode" "-interaction=nonstopmode" file-name)))
 
-(require 'reftex)
 
 ;; apply LaTeX hooks (spellcheck, etc.)
 (add-hook 'LaTeX-mode-hook (lambda ()
                              (flyspell-mode)
                              (outline-minor-mode)
                              (turn-on-auto-fill)
-                             (turn-on-reftex)
                              (setq latex-mode-map LaTeX-mode-map)
                              (yas/reload-all)
+                             (orgtbl-mode)
                              (flymake-mode)))
+
+
+(defun orgtbl-to-latex-matrix (table params)
+  "Convert the Orgtbl mode TABLE to a LaTeX Matrix."
+  (interactive)
+  (let* ((params2
+          (list
+           :tstart (concat "\\[\n\\begin{bmatrix}")
+           :tend "\\end{bmatrix}\n\\]"
+           :lstart "" :lend " \\\\" :sep " & "
+           :efmt "%s\\,(%s)" :hline "\\hline")))
+    (orgtbl-to-generic table (org-combine-plists params2 params))))
+
+
+(defun orgtbl-insert-matrix ()
+  "Insert a radio table template appropriate for this major mode."
+  (interactive)
+  (let* ((txt orgtbl-latex-matrix-string)
+         name pos)
+    (setq name (read-string "Table name: "))
+    (while (string-match "%n" txt)
+      (setq txt (replace-match name t t txt)))
+    (or (bolp) (insert "\n"))
+    (setq pos (point))
+    (insert txt)
+    ;; (goto-char pos)))
+    (previous-line)
+    (previous-line)))
+
+(defcustom orgtbl-latex-matrix-string  "% BEGIN RECEIVE ORGTBL %n
+% END RECEIVE ORGTBL %n
+\\begin{comment}
+#+ORGTBL: SEND %n orgtbl-to-latex-matrix :splice nil :skip 0
+
+\\end{comment}\n"
+  "Template for the latex matrix orgtbl translator
+All occurrences of %n in a template will be replaced with the name of the
+table, obtained by prompting the user."
+  :type 'string
+  :group 'org-table)
+
+
 
 
 ;; Autopair functionality in LaTeX mode
@@ -35,7 +76,7 @@
                    (list #'autopair-default-handle-action
                          #'autopair-latex-mode-paired-delimiter-action))))
 
-;; Add $ to the autopar list for Latex-mode
+;; Add $ to the autopair list for Latex-mode
 ;; (add-hook 'latex-mode-hook
 ;;           #'(lambda ()
 ;;               (push '(?$ . ?$)
